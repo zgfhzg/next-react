3024063d015919a76159e48548222c12d4861392
seokhyun <zgfhzg@gmail.com>
2025-03-19 22:30:53 +0900

    경로 설정 적용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                destination: "http://localhost/:path*",
+            }
+        ]
+    }
 };
 
 export default nextConfig;
