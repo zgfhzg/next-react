87ab762517dd7a1243a27ed2cbee96e9ff051912
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2025-07-17 10:16:33 +0900

    테이블 헤더 너비 조정

--- app/budget/columns.tsx
+++ app/budget/columns.tsx
@@ -8,8 +8,8 @@ export type Data = {    // 데이터 구조체
 }
 
 export const columns: ColumnDef<Data>[] = [
-    { header: '항목', accessorKey: 'name' },
-    { header: '금액', accessorKey: 'price' },
-    { header: '날짜', accessorKey: 'payDate' },
-    { header: '결제자', accessorKey: 'payer' },
+    { header: '항목', accessorKey: 'name', size: 150 },
+    { header: '금액', accessorKey: 'price', size: 150 },
+    { header: '날짜', accessorKey: 'payDate', size: 120 },
+    { header: '결제자', accessorKey: 'payer', size: 80 },
 ];
