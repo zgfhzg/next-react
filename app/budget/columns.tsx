1283b01c6c923c4ab362b45e8824601d70e7e370
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2025-07-17 15:08:32 +0900

    Update columns.tsx

--- app/budget/columns.tsx
+++ app/budget/columns.tsx
@@ -9,7 +9,7 @@ export type Data = {    // 데이터 구조체
 
 export const columns: ColumnDef<Data>[] = [
     { header: '항목', accessorKey: 'name', size: 150 },
-    { header: '금액', accessorKey: 'price', size: 150 },
-    { header: '날짜', accessorKey: 'payDate', size: 120 },
+    { header: '금액', accessorKey: 'price', size: 100 },
+    { header: '날짜', accessorKey: 'payDate', size: 150 },
     { header: '결제자', accessorKey: 'payer', size: 80 },
 ];
